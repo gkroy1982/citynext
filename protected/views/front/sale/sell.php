5bb03d8d0a3d76d4974f446bc58fc49be86a4adc
akashjain12366 <akashjain12366@rediffmail.com>
2016-08-02 01:14:35 +0530

    Used Products Category Listing
    Used Products Category Listing

--- protected/views/front/sale/sell.php
+++ protected/views/front/sale/sell.php
@@ -6,17 +6,20 @@ $url = Yii::app()->theme->baseUrl;
 ?>
 
   <div id="container">
-   <?php $this->renderPartial('left');?>
+    <aside class="sidebar col-lg-3 col-md-3 col-sm-3 col-xs-12">
+      <?php $this->renderPartial('left');?>
+    </aside>
     <!--Middle Part Start-->
-    <div id="content">
-      <!--<div class="slider-wrapper">
+    <section class="main-content col-lg-9 col-md-9 col-sm-9 col-xs-12">
+      <div id="content">
+        <!--<div class="slider-wrapper">
         <div id="slideshow" class="nivoSlider"> 
         <?php 
           $url_ads = Yii::app()->baseUrl.'/upload/ads/'; 
           foreach ($ads as $ad )
           {
             ?>
-                <a href="#"><img src="<?php echo $url_ads.$ad->image; ?>"/></a>                   
+                <a href=""><img src="<?php echo $url_ads.$ad->image; ?>"/></a>                   
                <?php
           } ?> 
         </div>
@@ -26,57 +29,72 @@ $url = Yii::app()->theme->baseUrl;
             $('#slideshow').nivoSlider();
         });
       </script>-->
-      <!--Slideshow Part End-->
-      <!--Featured Product Part Start-->
-    
-      <?php $this->renderPartial('post_sell');?>
-      
-      <div class="box">
-            
-			
-			<div class="box-heading"><div class="breadcrumb"> <?php  echo $nav;?></div></div>
-            <div class="box-content"> 
-              <div class="box-product">
+        <!--Slideshow Part End-->
+        <!--Featured Product Part Start-->
+        <div style="margin:20px 0 5px;">
+          <?php $this->renderPartial('post_sell');?>
+        </div>
+        <div class="box">
 
-                 <?php 
+          <div class="box-heading">
+            <div class="breadcrumb">
+              <?php  echo $nav;?>
+            </div>
+          </div>
+          <div class="box-content">
+            <div class="box-product subcategories">
+
+              <?php 
                  $url_img = Yii::app()->basePath.'/../upload/icons/'; 
                  $p_url=Yii::app()->baseUrl.'/upload/icons/';
                   foreach($categories as $category)
                   {
-                    ?><div style='width:155px;height:150px;'>
-                    <a href="<?php echo Yii::app()->createUrl('sale/products',array('id'=>$category->mcid));?>">
-                        <?php if($category->icon!='' and file_exists($url_img.$category->icon) ){?>
-                          <div class="image">
-                              <img style='width:152px;height:125px;' src="<?php echo $p_url.$category->icon;?>"  />
-                          </div> 
-                          <?php } else {?>
+                    ?>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 subcategory">
+                  <a href="<?php echo Yii::app()->createUrl('sale/products',array('id'=>$category->mcid));?>">
+                    <?php if($category->icon!='' and file_exists($url_img.$category->icon) ){?>
+                      <img src="<?php echo $p_url.$category->icon;?>" alt="product">
+                      <?php } else {?>
+                        <img src="<?php echo $p_url.'images.jpg';?>" alt="product">
+                        <?php } ?>
+                          <div class="name" style="text-align:center;">
 
-                          <div class="image">
-                              <img style='width:152px;height:125px;' src="<?php echo $p_url.'images.jpg';?>"  />
-                          </div> 
+                          </div>
+                          <div class="product-info">
+                            <h6><?php echo substr(ucwords($category->category),0,20);?>..</h6>
+                          </div>
+                  </a>
 
-                          <?php } ?>
 
+                  <!--
+<a href="<?php echo Yii::app()->createUrl('sale/products',array('id'=>$category->mcid));?>">
+  <?php if($category->icon!='' and file_exists($url_img.$category->icon) ){?>
+    <div class="image">
+      <img style='width:152px;height:125px;' src="<?php echo $p_url.$category->icon;?>" />
+    </div>
+    <?php } else {?>
 
-                          
-                          <div class="name" style="text-align:center;"> 
-                            
-                            <?php echo substr(ucwords($category->category),0,20);?>..
-                          </div>             
-                        
-                    </a></div>
-                    <?php
+      <div class="image">
+        <img style='width:152px;height:125px;' src="<?php echo $p_url.'images.jpg';?>" />
+      </div>
+
+      <?php } ?>
+        <div class="name" style="text-align:center;">
+          <?php echo substr(ucwords($category->category),0,20);?>..
+        </div>
+</a>
+-->
+                </div>
+                <?php
                   }
                   ?>
-
-                </div>
             </div>
           </div>
+        </div>
+      </div>
+    </section>
 
-     
-    </div>
-
-      <div class="clear"></div>
-      <div class="social-part"></div>
-    </div>
-    <!--Middle Part End-->
+    <div class="clear"></div>
+    <div class="social-part"></div>
+  </div>
+  <!--Middle Part End-->
