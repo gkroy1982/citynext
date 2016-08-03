1e28a0869ddc4040619fb8e4bb547106ee53ddde
akashjain12366 <akashjain12366@rediffmail.com>
2016-08-03 23:48:40 +0530

    Todays Offer Page Complete
    Todays Offer Page Complete

--- protected/views/front/sale/products.php
+++ protected/views/front/sale/products.php
@@ -10,9 +10,8 @@ $url = Yii::app()->theme->baseUrl;
       <aside class="sidebar col-lg-3 col-md-3 col-sm-3 col-xs-12">
         <?php $this->renderPartial('left');?>
       </aside>
-      <!--Middle Part Start-->
-      <section class="main-content col-lg-9 col-md-9 col-sm-9 col-xs-12">
-        <!--Middle Part Start-->
+<!--Middle Part Start-->
+<section class="main-content col-lg-9 col-md-9 col-sm-9 col-xs-12">
         <div id="content">
 
           <!--Slideshow Part Start-->
