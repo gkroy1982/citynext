b4644df7379f05c4cb88e5cb0261b2518c15281b
akashjain12366 <akashjain12366@rediffmail.com>
2016-08-10 00:10:31 +0530

    Committing for footer pages
    Roy Please see why accordion not closing... I m unable to do that

--- protected/views/front/site/contact-us.php
+++ protected/views/front/site/contact-us.php
@@ -1,53 +1,65 @@
 <?php $this->pageTitle='Jhansishopping.com | Contact Us';?>
-  <div id="container">
-   <?php $this->renderPartial('left');?>
+  <div id="container" class="content">
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+      <?php $this->renderPartial('left');?>
+    </div>
     <!--Middle Part Start-->
+    <section class="main-content col-lg-9 col-md-9 col-sm-9 col-xs-12">
+      <div id="content">
+        <!--Breadcrumb Part Start-->
+        <div class="breadcrumb"> <a href="<?php echo Yii::app()->createUrl('site/index')?>">Home</a> » <a href="#">Contact Us</a> </div>
+        <!--Breadcrumb Part End-->
+        <div class="row">
 
-    <div id="content">
-      <!--Breadcrumb Part Start-->
-      <div class="breadcrumb"> <a href="<?php echo Yii::app()->createUrl('site/index')?>">Home</a> » <a href="#">Contact Us</a> </div>
-      <!--Breadcrumb Part End-->
-      <h1>Contact Us</h1>
-      <h2>Our Location</h2>
-      <div class="contact-info">
-        <div class="content">
-          <div class="left">
-            <h4><b>Address:</b></h4>
-            <p>401, Sample Plaza, New Way Road, Sample Address, Maharashtra<br>
-              Address 1</p>
-          </div>
-          <div class="right">
-            <h4><b>Telephone:</b></h4>
-            123456789<br>
-            <br>
-			
-			<h4><b>Email Id:</b></h4>
-			<table>
-			<tr>
-			<th> For Info </th>
-			<td>: <a href="mailto:info@jhansishopping.com">info@jhansishopping.com</a> </td>
-			</tr>
-			<tr>
-			<th> For Sales </th>
-			<td>: <a href="mailto:sales@jhansishopping.com">sales@jhansishopping.com</a></td>
-			</tr>
-			<tr>
-			<th> For Support </th>
-			<td>: <a href="mailto:support@jhansishopping.com">support@jhansishopping.com</a></td>
-			</tr>
-			<tr>
-			<th> For Jobs </th>
-			<td>: <a href="mailto:careers@jhansishopping.com">careers@jhansishopping.com</a></td>
-			</tr>
-			
-			</table>
-            
+          <div class="col-lg-12 col-md-12 col-sm-12">
+
+            <div class="carousel-heading no-margin">
+              <h4>Contact Information</h4>
+            </div>
+
+            <div class="page-content contact-info">
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.463879414711!2d73.82243021441721!3d18.553113973021517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf0eed1beca7%3A0xf07ab6b613c2af51!2sSavitribai+Phule+Pune+University!5e0!3m2!1sen!2sin!4v1466272422006" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+              <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                  <div class="contact-item green">
+                    <i class="icons icon-location-3"></i>
+                    <p>401, Sample Plaza, New Way Road, Sample Address, Maharashtra </p>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                  <div class="contact-item blue">
+                    <i class="icons icon-mail"></i>
+                    <p>
+                      <a href="mailto:info@jhansishopping.com">info@jhansishopping.com</a>
+                      <br>
+                      <a href="mailto:sales@jhansishopping.com">sales@jhansishopping.com</a>
+                    </p>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                  <div class="contact-item orange">
+                    <i class="icons icon-phone"></i>
+                    <p>123456789
+                      <br>
+                    </p>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                  <div class="contact-item blue">
+                    <i class="icons icon-mail"></i>
+                    <p>
+                      <a href="mailto:support@jhansishopping.com">support@jhansishopping.com</a>
+                      <br>
+                      <a href="mailto:careers@jhansishopping.com">careers@jhansishopping.com</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    <!--  <h2>Contact Form</h2>-->
-
-<!--
+        <!--
 <?php 
 
 
@@ -120,9 +132,10 @@ $form=$this->beginWidget('CActiveForm', array(
 <?php $this->endWidget();
 
 } ?> -->
-    <!--Middle Part End-->
-    <div class="clear"></div>
-    <div class="social-part">
-     
-    </div>
-  </div>
+        <!--Middle Part End-->
+        <div class="clear"></div>
+        <div class="social-part">
+
+        </div>
+      </div>
+    </section>
